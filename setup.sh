380737109447b4dc41dc5b21915d6c2403fbb391
masakiishii <masaki.ishii511@gmail.com>
2014-11-01 00:08:30 +0900

    add setup.sh

---
+++ setup.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+
+mkdir_if_not_exist () {
+    local dir=$1
+    if [ ! -d $dir ]; then
+        mkdir -p $dir
+    fi
+}
+
+# Emacs
+mkdir_if_not_exist ~/.emacs.d
+
+EMACSDIR=${HOME}/.emacs.d
+CURRENTDIR=`pwd`
+
+cp ${CURRENTDIR}/.emacs.el ${HOME}/
+
+emacsFileList=("init.el" "init_loader" "elisps" "Cask")
+
+for i in ${emacsFileList[*]}
+do
+    ln -sf ${CURRENTDIR}/$i ${EMACSDIR}
+done
+
+# install elisp
+cd ${EMACSDIR} && cask install
+
