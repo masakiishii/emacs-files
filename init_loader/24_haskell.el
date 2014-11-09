4818edf2916ea37807ac06fcc47cf4b157945de9
masakiishii <masaki.ishii511@gmail.com>
2014-11-09 16:20:44 +0900

    fixed haskell-mode indentation

--- init_loader/24_haskell.el
+++ init_loader/24_haskell.el
@@ -2,4 +2,6 @@
 (require 'haskell-cabal)
 
 (add-to-list 'auto-mode-alist '("\\.hs$" . haskell-mode))
-
+(add-hook 'haskell-mode-hook 'turn-on-haskell-simple-indent)
+(add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
+(add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
