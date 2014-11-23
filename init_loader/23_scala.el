d68f5662c67449c8c0875b341ca237b62143d9ab
masakiishii <masaki.ishii511@gmail.com>
2014-11-24 00:06:56 +0900

    add init_loader/23_scala.el

---
+++ init_loader/23_scala.el
@@ -0,0 +1,13 @@
+;;; 23_scala.el ---
+
+;; Copyright (C) 2014  masaki
+
+;; Author: masaki <masaki@masaki-no-MacBook-Air.local>
+;; Keywords:
+
+(require 'scala-mode2)
+(require 'ensime)
+
+(add-hook 'scala-mode-hook 'ensime-scala-mode-hook)
+(add-to-list 'auto-mode-alist
+             '("\\.\\(scala\\|sbt\\)\\'" . scala-mode))
