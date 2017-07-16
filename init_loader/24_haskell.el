f4787f0f93aab1e4b7decd51f4a1db9c62229a17
masakiishii <masaki.ishii511@gmail.com>
2017-07-17 08:26:57 +0900

    haskell mode indentation 4

--- init_loader/24_haskell.el
+++ init_loader/24_haskell.el
@@ -6,6 +6,11 @@
 (add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
 (add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
 
+(custom-set-variables
+ '(haskell-indent-after-keywords (quote (("where" 4 0) ("of" 4) ("do" 4) ("mdo" 4) ("rec" 4) ("in" 4 0) ("{" 4) "if" "then" "else" "let")))
+ '(haskell-indent-offset 4)
+ '(haskell-indent-spaces 4))
+
 (require 'haskell-mode)
 (define-key haskell-mode-map "\C-ch" 'haskell-hoogle)
 ;(setq haskell-hoogle-command "hoogle")
