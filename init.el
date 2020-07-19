71179075e44fef49cad48c057596b4328f85b230
masakiishii <masaki.ishii511@gmail.com>
2020-07-19 22:41:26 +0900

    remove unnecessary package

--- init.el
+++ init.el
@@ -1,10 +1,4 @@
 ;; recursive loading elisp files under .emacs.d directory
-(let ((default-directory (expand-file-name "~/.emacs.d/elisps")))
- (add-to-list 'load-path default-directory)
- (if (fboundp 'normal-top-level-add-subdirs-to-load-path)
-     (normal-top-level-add-subdirs-to-load-path)))
-
-;; for cask
 (let ((default-directory (expand-file-name "~/.emacs.d/.cask")))
  (add-to-list 'load-path default-directory)
  (if (fboundp 'normal-top-level-add-subdirs-to-load-path)
