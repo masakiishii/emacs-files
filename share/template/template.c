e8e06e96ae3b24e187f2ac614256c61e50513aac
masakiishii <masaki.ishii511@gmail.com>
2022-01-09 00:13:36 +0900

    modified template

--- share/template/template.c
+++ share/template/template.c
@@ -2,9 +2,11 @@
 using namespace std;
 
 #define rep(i,n) for (int i = 0; i < (n); ++i)
-#define drep(i,n) for (int i = (n)-1; i >= 0; --i)
-
+#define rep1(i,n) for(int i = 1; i <= (n); ++i)
+#define drep(i,n) for(int i = (n)-1; i >= 0; --i)
+#define srep(i,s,t) for (int i = s; i < (t); ++i)
 using ll = long long;
+using ld = long double;
 using uint = unsigned;
 using ull = unsigned long long;
 using P = pair<int,int>;
@@ -15,7 +17,6 @@ const int INF = 1001001001;
 class solver {
 public:
 	void solve() {
-		return;
 	}
 };
 
